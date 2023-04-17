18956d800f6f632304a93d0fb0e6c0034fad95ac
Roma Lomachinskiy <54740014+RomaLoma91@users.noreply.github.com>
2023-04-17 14:32:04 +0300

    clean_folder

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name='clean_folder',
+    version='0.1',
+    description='scripts clean folder and normalize file name',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+      ],
+    author='loma',
+    author_email='lomachinskiy.r@gmail.com',
+    license='MIT',
+    packages=['clean_folder'],
+    install_requires=[],
+    python_requires='>=3.5',
+    include_package_data=True
+)
